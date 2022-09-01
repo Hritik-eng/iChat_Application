bb83d38495d2ad1cba4e03df08d49f90143cd425
Hritik Gupta <hritikgupta895@gmail.com>
2022-09-01 10:16:44 +0530

    change read.me file

--- src/db/conn.js
+++ src/db/conn.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 
 
 mongoose
-  .connect("mongodb+srv://HriTech:Mongodb123@cluster0.pluvvyc.mongodb.net/iChat")
+  .connect( "mongodb+srv://HriTech:Mongodb123@cluster0.pluvvyc.mongodb.net/iChat")
   .then(() => {
     console.log("Successfully connected");
   })
