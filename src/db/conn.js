64ea10680b16e1f450c22cb94a523e79f0c35d7e
Hritik Gupta <69283881+Hritik-eng@users.noreply.github.com>
2022-09-14 16:23:59 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,24 +1,5 @@
 //require("dotenv").config();
 const mongoose = require("mongoose");
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/iChat")
-//   .then(() => {
-//     console.log("Successfully connected");
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
-
-
-//mongoose
- // .connect(process.env.MongoDB_URL)
- // .then(() => {
- //   console.log("Successfully connected");
- // })
-// .catch((e) => {
-    //console.log(e);
- // });
-
 
 mongoose
   .connect("mongodb+srv://HriTech:Mongodb123@cluster0.pluvvyc.mongodb.net/iChat")
